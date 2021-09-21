96cc4638f6bd88f6ed83512b8d7d0f6ee4404156
Your Name <827511@mystma.org>
2021-09-21 15:16:37 -0500

    3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(a, b, c, d, e):
-    return a + " ", b + " ", c + " ", d + " ", e
+    return a + " " b + " " c + " " d + " " + e
 
 print(topFiveMovies("Raiders of the lost ark", "Once upon a time in hollywood", "spiderman 2", "Once upon a time in the west", "There will be blood"))
